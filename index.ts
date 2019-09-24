66b8fe8be7279f147bdd923b28b9e108dcb8055a
Hasan uz zaman Sheikh Mohammad <msheikh@microsoft.com>
2019-09-24 13:02:20 -0700

    observable and subscription difference

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ subject.subscribe({
   next: (v) => console.log(`observerB: ${v}`)
 });
 
-subject.next(1);
+subject.next(Math.random());
 subject.next(2);
 
 // Logs:
@@ -18,32 +18,7 @@ subject.next(2);
 // observerA: 2
 // observerB: 2
 
-/*
-import { Observable } from 'rxjs';
 
-const observable = new Observable(subscriber => {
-  subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next(3);
-  setTimeout(() => {
-    subscriber.next(4);
-    subscriber.complete();
-  }, 1000);
-});
-
-console.log('just before subscribe');
-observable.subscribe({
-  next(x) { console.log('got value ' + x); },
-  error(err) { console.error('something wrong occurred: ' + err); },
-  complete() { console.log('done'); }
-});
-observable.subscribe({
-  next(x) { console.log('2nd got value ' + x); },
-  error(err) { console.error('something wrong occurred: ' + err); },
-  complete() { console.log('done'); }
-});
-console.log('just after subscribe');
-*/
 /*
 // basic example
 var { of } = require('rxjs');
---
+++ observable1.ts
@@ -0,0 +1,22 @@
+import { Observable } from 'rxjs';
+
+const observable = new Observable(subscriber => {
+  subscriber.next(Math.random());
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
+});
+
+console.log('just before subscribe');
+observable.subscribe({
+  next(x) { console.log('got value ' + x); },
+  error(err) { console.error('something wrong occurred: ' + err); },
+  complete() { console.log('done'); }
+});
+observable.subscribe({
+  next(x) { console.log('2nd got value ' + x); },
+  error(err) { console.error('something wrong occurred: ' + err); },
+  complete() { console.log('done'); }
+});
+console.log('just after subscribe');
